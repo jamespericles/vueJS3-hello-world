5fca11b687481e99e764b68706ff45ce4273a8f2
jamespericles <63256479+jamespericles@users.noreply.github.com>
2021-02-24 14:19:30 -0500

    testing different features of vue

---
+++ app.js
@@ -0,0 +1,56 @@
+const app = Vue.createApp({
+  // data, functions
+  data() {
+    return {
+      showBooks: true,
+      books: [
+        {
+          title: "Name of the Wind",
+          author: "Patrick Rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "The Way of Kings",
+          author: "Brandon Sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "The Final Empire",
+          author: "Brandon Sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
+      x: 0,
+      y: 0,
+      url: "https://github.com/jamespericles",
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
+    },
+  },
+});
+
+app.mount("#app");
